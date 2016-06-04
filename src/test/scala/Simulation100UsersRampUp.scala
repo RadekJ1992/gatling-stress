01086932e769842a9fa1ac3ac8cf52e4eff91e7f
Radoslaw Jarzynka <radoslaw.jarzynka@gmail.com>
2016-06-04 17:29:50 +0200

    testy dodane

--- src/test/scala/Simulation100UsersRampUp.scala
+++ src/test/scala/Simulation100UsersRampUp.scala
@@ -1,8 +1,7 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import scala.concurrent.duration._
 
-class Simulation10UsersAtOnce extends Simulation {
+class Simulation100UsersRampUp extends Simulation {
 
   val httpConf = http
     .baseURL("http://localhost:8080")
@@ -12,12 +11,14 @@ class Simulation10UsersAtOnce extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("Simulation 1 - 10 users at once")
+  val scn = scenario("Simulation 2 - 100 users over 15 seconds")
     .exec(http("index.html")
     .get("/"))
     .pause(2)
 
   setUp(
-    scn.inject(atOnceUsers(10))
+    scn.inject(
+      rampUsers(100).over(10)
+    )
   ).protocols(httpConf)
 }
