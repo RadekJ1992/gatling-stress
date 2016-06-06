5cb6dfad6040405bd902417d110177bb26bb121c
Radoslaw Jarzynka <radoslaw.jarzynka@gmail.com>
2016-06-06 21:23:00 +0200

    ostateczne przygotowanie do prezentacji

--- src/test/scala/EpointSimulation.scala
+++ src/test/scala/EpointSimulation.scala
@@ -1,7 +1,8 @@
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import scala.concurrent.duration._
 
-class Simulation100UsersRampUp extends Simulation {
+class EpointSimulation extends Simulation {
 
   val httpConf = http
     .baseURL("http://localhost:8080")
@@ -11,14 +12,12 @@ class Simulation100UsersRampUp extends Simulation {
     .acceptEncodingHeader("gzip, deflate")
     .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
 
-  val scn = scenario("Simulation 2 - 100 users over 15 seconds")
+  val scn = scenario("Simulation")
     .exec(http("index.html")
-    .get("/"))
+      .get("/"))
     .pause(2)
 
   setUp(
-    scn.inject(
-      rampUsers(100).over(10)
-    )
+    scn.inject(atOnceUsers(10))
   ).protocols(httpConf)
 }
